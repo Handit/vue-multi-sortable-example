28b59be73d84f2159152382b88c1199978b12b7d
lucas_amaral <lucas.amaral.dw@gmail.com>
2018-02-26 19:58:39 -0300

    'deploy'

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-    fs.readFile("./dist/index.html", 'utf8', function(err, data) {
+    fs.readFile(__dirname + "/dist/index.html", 'utf8', function(err, data) {
         if (err) throw err;
         res.send(data);
     });
 });
 
-app.use('/static', express.static('dist/static'));
+app.use('/static', express.static(__dirname + '/dist/static'));
 
 app.listen(process.env.PORT || 3000, function() {
     console.log("listening on port " + process.env.PORT || 3000)
